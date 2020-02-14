273aa51949fe07ca5919bb89dc4a6df88b07300d
Samar Omar <samaromm98@gmail.com>
2020-02-14 20:48:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,4 +26,7 @@ class Evergreen extends Tree{
     super(species)
     this.name=name
   }
+  static definition(){
+    return super.definition+ 'Deciduous trees shed their leaves annually.'
+  }
 }
